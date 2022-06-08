720c8d125eabb593daf48c686e84709342611557
Beto Najera <ing.betonajera@gmail.com>
2022-06-08 16:16:48 -0500

    :sparkles: feat: Add Home view
    Add home view

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Searcher from '../components/Searcher.vue'
+
+export default defineComponent({
+	components: { Searcher },
+	setup() {},
+})
+</script>
+
+<template lang="pug">
+section.title
+  h1 Pokédex
+searcher
+</template>
+
+<style lang="postcss" scoped>
+.title {
+	@apply text-dark-light;
+	@apply mx-40;
+	@apply py-5;
+	@apply px-10;
+}
+
+.title > h1 {
+	@apply text-3xl;
+}
+</style>
