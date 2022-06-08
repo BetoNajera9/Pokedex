02ac5f90ae191162a67de4e05f554ab5e1193118
Beto Najera <ing.betonajera@gmail.com>
2022-06-08 16:17:23 -0500

    :sparkles: feat: Add Favorites view
    Add favorites view

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,11 @@ const routes: Array<RouteRecordRaw> = [
 		name: 'home',
 		component: () => import('../views/Home.vue'),
 	},
+	{
+		path: '/favorites',
+		name: 'favorites',
+		component: () => import('../views/Favorites.vue'),
+	},
 ]
 
 const router = createRouter({
