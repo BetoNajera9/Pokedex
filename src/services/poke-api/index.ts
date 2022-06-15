0369f5c1bf5efcddd237c2ccc55ec133d315d00e
Beto Najera <ing.betonajera@gmail.com>
2022-06-15 04:13:17 -0500

    :sparkles: feat: Add more props to poke-api return
    Add more prop to poke-api return

--- src/services/poke-api/index.ts
+++ src/services/poke-api/index.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 import { Pokemon } from './models'
 import { useStore } from '../../store'
 import { ActionTypes } from '../../store/actions'
+import { PokemonStats } from './models/pokemon-stats.model'
 
 export default class PokeApi {
 	private client: AxiosInstance
@@ -32,6 +33,8 @@ export default class PokeApi {
 
 		const { data: pokemonsData } = await this.client.get(url)
 
+		const pokemonSpecies = await this.getSpecies(pokemonsData.species.url)
+
 		await Promise.all(
 			pokemonsData.types.map(async (element: any) => {
 				const dataType = await this.getTypes(element.type.url)
@@ -43,6 +46,20 @@ export default class PokeApi {
 			})
 		)
 
+		const stats: PokemonStats = {} as PokemonStats
+
+		pokemonsData.stats.forEach((element: any) => {
+			const a = []
+			for (let index = 15; index >= 0; index--) {
+				if (index < Math.round(element.base_stat / 15)) a.push('fill')
+				else a.push('empty')
+			}
+			stats[element.stat.name] = {
+				number: element.base_stat,
+				array: a,
+			}
+		})
+
 		return {
 			id: pokemonsData.id,
 			name: pokemonsData.name,
@@ -50,6 +67,11 @@ export default class PokeApi {
 			img: pokemonsData.sprites.other['official-artwork'].front_default,
 			height: pokemonsData.height,
 			weight: pokemonsData.weight,
+			desciption: pokemonSpecies.description,
+			evolution_chain: pokemonSpecies.evolution_chain,
+			category: pokemonSpecies.category,
+			ability: pokemonsData.abilities[0].ability.name,
+			stats,
 		}
 	}
 
@@ -57,5 +79,23 @@ export default class PokeApi {
 		const { data: typesData } = await this.client.get(url)
 
 		return typesData.damage_relations.double_damage_from
+	}
+
+	private async getSpecies(url: string) {
+		const { data: speciesData } = await this.client.get(url)
+
+		const description = speciesData.flavor_text_entries[0].flavor_text
+
+		const evolutionArray = speciesData.evolution_chain.url.split('/')
+
+		const category = speciesData.genera.find(
+			(element: any) => element.language.name === 'en'
+		)
+
+		return {
+			description,
+			evolution_chain: evolutionArray[evolutionArray.length - 2],
+			category: category.genus,
+		}
 	}
 }
