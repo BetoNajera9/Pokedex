7562a206792ebbafa6d4d75c4cfea4d09fa32bdb
Beto Najera <ing.betonajera@gmail.com>
2022-06-15 04:07:51 -0500

    :sparkles: feat: Add pokemonImage component
    Add pokemonImage component

---
+++ src/components/PokemonImage.vue
@@ -0,0 +1,68 @@
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import { ActionTypes } from '../store/actions'
+import { useStore } from '../store'
+
+export default defineComponent({
+	props: ['imgSrc', 'favorite', 'pokemonName'],
+	setup(props) {
+		const { favorite, imgSrc, pokemonName } = props
+
+		const store = useStore()
+
+		let isFavorite = ref(false)
+
+		const toogleFavorite = () => {
+			isFavorite.value = !isFavorite.value
+
+			if (isFavorite.value === false) {
+				store.dispatch(ActionTypes.RemovePokemonFavorite, pokemonName)
+			} else {
+				const pokemonData = store.getters.pokemon(pokemonName)
+				store.dispatch(ActionTypes.SetPokemonFavorite, pokemonData)
+			}
+		}
+
+		onMounted(async () => {
+			const initialIsFavorite = store.getters.favoritePokemon(pokemonName)
+			isFavorite.value = !initialIsFavorite ? false : true
+		})
+
+		return {
+			pokemonName,
+			toogleFavorite,
+			isFavorite,
+			favorite,
+			imgSrc,
+		}
+	},
+})
+</script>
+
+<template lang="pug">
+div
+	span(@click="toogleFavorite")
+		mdicon.btn-favorite(:class="{favorite: isFavorite}" v-show="favorite === true"  name="heart" size="25")
+	router-link(:to="{ name: 'pokemonDetails', params: { name: pokemonName }}")
+		img(:src="imgSrc")
+
+</template>
+
+<style lang="postcss" scoped>
+div span {
+	@apply flex;
+	@apply justify-end;
+}
+
+.favorite.mdi {
+	@apply text-red;
+}
+
+.btn-favorite {
+	@apply block;
+	@apply text-dark;
+	@apply cursor-pointer;
+	@apply mr-2;
+	@apply mt-2;
+}
+</style>
